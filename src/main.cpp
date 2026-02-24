4815456f51927c043cb900d15e8a6eca9575dfb3
bencejuhaasz <32618702+bencejuhaasz@users.noreply.github.com>
2026-02-24 12:55:03 +0000

    base of a server

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <csignal>
+#include <thread>
+#include <netinet/in.h>
+
+const int PORT = 8080;
+
+void sighandler(int signal) {
+    std::cout << "cppserver: received signal " << signal << std::endl;
+}
 
 int main() {
     std::cout << "cppserver: starting server..." << std::endl;
+    std::signal(SIGINT, sighandler);
+    //std::thread input_thread(input_thread_func); #start new thread for handling user input
+    int new_socket;
+    sockaddr_in address{};
+    int addrlen = sizeof(address);
     while (true)
     {
         int place_holder = 0;
