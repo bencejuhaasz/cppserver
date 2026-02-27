c91c77763f11c3655889ff6d3716af93f4060a67
bencejuhaasz <32618702+bencejuhaasz@users.noreply.github.com>
2026-02-27 13:33:58 +0000

    spread threads across cpu cores

--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -1,5 +1,9 @@
 #include "thread_pool.h"
 #include <iostream>
+#include <pthread.h>
+#include <sched.h>
+#include <unistd.h>
+#include <thread>
 
 ThreadPool::ThreadPool(size_t numThreads)
     : stopping(false), numThreads(numThreads) {}
@@ -10,8 +14,21 @@ ThreadPool::~ThreadPool() {
 
 void ThreadPool::start() {
     stopping = false;
+    unsigned int ncores = std::thread::hardware_concurrency();
+    if (ncores == 0) ncores = static_cast<unsigned int>(sysconf(_SC_NPROCESSORS_ONLN));
+    if (ncores == 0) ncores = 1;
+
     for (size_t i = 0; i < numThreads; ++i) {
         workers.emplace_back([this, i]() { workerLoop(static_cast<int>(i)); });
+
+        // Pin the newly-created thread to a specific CPU core in round-robin fashion
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(static_cast<int>(i % ncores), &cpuset);
+        int rc = pthread_setaffinity_np(workers.back().native_handle(), sizeof(cpu_set_t), &cpuset);
+        if (rc != 0) {
+            std::cerr << "ThreadPool: failed to set affinity for thread " << i << " rc=" << rc << std::endl;
+        }
     }
 }
 
